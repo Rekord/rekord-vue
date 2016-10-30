1639f29a5ef2e7a191c71f0aa572b234fb1f4380
Philip Diffenderfer <pdiffenderfer@gmail.com>
2016-10-30 04:19:30 -0400

    Switched to UMD.

--- build/rekord-vue.js
+++ build/rekord-vue.js
@@ -1,34 +1,60 @@
-/* rekord-vue 1.4.0 - A rekord binding to Vue.js by Philip Diffenderfer */
-(function(global, undefined)
+/* rekord-vue 1.4.1 - A rekord binding to Vue.js by Philip Diffenderfer */
+// UMD (Universal Module Definition)
+(function (root, factory)
 {
-  var Rekord = global.Rekord;
-  var Collection = Rekord.Collection;
-  var replaceMethod = Rekord.replaceMethod;
+  if (typeof define === 'function' && define.amd) // jshint ignore:line
+  {
+    // AMD. Register as an anonymous module.
+    define(['Rekord'], function(Rekord) { // jshint ignore:line
+      return factory(root, Rekord);
+    });
+  }
+  else if (typeof module === 'object' && module.exports)  // jshint ignore:line
+  {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(global, require('Rekord'));  // jshint ignore:line
+  }
+  else
+  {
+    // Browser globals (root is window)
+    root.Rekord = factory(root, root.Rekord);
+  }
+}(this, function(global, Rekord, undefined)
+{
 
-  replaceMethod( Collection.prototype, 'trigger', function($trigger)
+  var Collection = Rekord.Collection;
+
+  Rekord.replaceMethod( Collection.prototype, 'trigger', function($trigger)
   {
     return function trigger(eventName, args)
     {
       var result = $trigger.apply(this, arguments);
       var ob = this.__ob__;
+
       if (ob)
       {
         switch (eventName)
         {
           case 'add':
-            ob.observeArray([args[0]]);
+            ob.observeArray(args);
             break;
           case 'adds':
             ob.observeArray(args[0]);
             break;
           case 'reset':
-            ob.observeArray(this.slice());
+            ob.observeArray(this);
             break;
         }
+
         ob.dep.notify();
       }
+
       return result;
     };
   });
 
-})(this);
+  return Rekord;
+
+}));
--- src/vue.js
+++ src/vue.js
@@ -1,6 +1,28 @@
-(function(global, undefined)
+// UMD (Universal Module Definition)
+(function (root, factory)
 {
-  var Rekord = global.Rekord;
+  if (typeof define === 'function' && define.amd) // jshint ignore:line
+  {
+    // AMD. Register as an anonymous module.
+    define(['Rekord'], function(Rekord) { // jshint ignore:line
+      return factory(root, Rekord);
+    });
+  }
+  else if (typeof module === 'object' && module.exports)  // jshint ignore:line
+  {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(global, require('Rekord'));  // jshint ignore:line
+  }
+  else
+  {
+    // Browser globals (root is window)
+    root.Rekord = factory(root, root.Rekord);
+  }
+}(this, function(global, Rekord, undefined)
+{
+
   var Collection = Rekord.Collection;
 
   Rekord.replaceMethod( Collection.prototype, 'trigger', function($trigger)
@@ -32,4 +54,6 @@
     };
   });
 
-})(this);
+  return Rekord;
+
+}));
